cfd601f12c17d54574ea2d9ac3211c8da5448246
Cantuğ Şahiner <cantugsahiner@gmail.com>
2025-12-02 19:42:41 +1100

    10 is done

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahrenheit) {
+
+  let toCelcius = 0;
+  toCelcius = ((Fahrenheit -32 ) * 5)  / 9;
+  let roundedString = toCelcius.toFixed(1);
+  let roundedCelcius = parseFloat(roundedString);
+  return roundedCelcius;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let toFahrenheit = 0;
+  let roundedFahrenheit;
+  let roundedStringF;
+  toFahrenheit = ((celcius * 9) / 5) + 32 ;
+  roundedStringF = toFahrenheit.toFixed(1);
+  roundedFahrenheit = parseFloat(roundedStringF);
+  return roundedFahrenheit;
 };
 
 // Do not edit below this line
