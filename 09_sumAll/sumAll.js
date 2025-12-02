4c3283762d4f50d12744e5e84a7886bac87d71a8
Cantug Sahiner <cantugsahiner@gmail.com>
2025-12-02 13:54:00 +1100

    09 is done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+
+    let min = Math.min(a,b);
+    let max = Math.max(a,b);
+    let sum = 0;
+
+    if(!Number.isInteger(a) || !Number.isInteger(b)){
+        return "ERROR";
+    }
+
+    while(min <= max){
+        sum += min;
+        min++;
+    } 
+    if(sum < 0){
+        return "ERROR";
+
+    }
+    else{
+        return sum;
+    }
+    
 
 };
 
